729df623c6954eec7979b02ff68d53dc74d0d306
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2022-08-10 21:42:55 -0300

    Removendo arquivos de tests e storybook

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Home } from "./components/Home";
+import { App } from "./components/App";
 import { GlobalStyles } from "./styles/global";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <App />
 
     <GlobalStyles />
   </React.StrictMode>
